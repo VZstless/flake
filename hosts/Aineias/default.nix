50a0bff94568ec35c10f41fba51e11d11dc7eb4f
VZstless <i@vzstless.moe>
2026-07-21 17:12:22 +0800

    delete part of comments

--- hosts/Aineias/default.nix
+++ hosts/Aineias/default.nix
@@ -16,10 +16,6 @@
     ];
 
   networking.hostName = "Aineias"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -104,12 +100,6 @@
   # List services that you want to enable:
 
   services.tailscale.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   services.openssh = {
     enable = true;
